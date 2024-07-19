89fc5825406f109e6c24b40777c03fd95df752ac
Casey <dinnerdoggy@gmail.com>
2024-07-18 21:13:45 -0500

    completed challenge and styled

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("connected")
+const llamaNamer = function () {
+    const possibleNames = ["Larry", "Leon", "Leona", "Les", "Laura", "Lemony", "Lars", "Lekisha"]
+    const randomizer = Math.floor(Math.random() * 7);
+
+    const namer = function () {
+        var suffix = " the Llama"
+        const name = possibleNames[randomizer]
+        return name + suffix;
+    }
+    return namer
+}
+
+nameMaker = llamaNamer()
+console.log(nameMaker())
+
+
+let domTarget = document.querySelector(".name");
+domTarget.innerHTML = nameMaker();
